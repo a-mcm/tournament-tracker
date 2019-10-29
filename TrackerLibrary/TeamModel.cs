f98d949c1fc6cb7a402eddcc4fa7ec858d5ec1c2
a-mcm <57138156+a-mcm@users.noreply.github.com>
2019-10-29 22:43:53 +1000

    Add files

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    class TeamModel
+    {
+        /// <summary>
+        /// A list of every player (PersonModel) in this particular team
+        /// </summary>
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        /// <summary>
+        /// Name of this team
+        /// </summary>
+       
+        public string TeamName { get; set; }
+    }
+}
